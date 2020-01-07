fc8910e2d557d07c282b91d68f9753fcfa01e6b3
HC Febrian <febriasnyah.online@gmail.com>
2020-01-07 21:37:02 +0700

    master 2

--- kemampuan-dasar/kemampuan-dasar-1/latihan/shellscripsts/MyScript.ps1
+++ kemampuan-dasar/kemampuan-dasar-1/latihan/shellscripsts/MyScript.ps1
@@ -1,1 +1,6 @@
 ﻿Write-Output 'Custom PowerShell profile in effect!'
+if (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{Write-Output 'Running as Administrator!'}
+else
+{Write-Output 'Running Limited!'}
+Pause
